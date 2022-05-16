dff478c122916ad778eb07fb28f785614cba7261
Daniel Rouleau <daniel.rouleau@se.com>
2022-05-16 14:59:44 -0600

    Add thumbnail

--- js/json.js
+++ js/json.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
   $.getJSON("gibson_songs.json", function (data) {
     $.each(data, function (i, val) {
-      $('#output').append('<p>Date: ' + data[i].uploadDate + '</p>');
+      $('#output').append('<p><img src="https://img.youtube.com/vi/' + data[i].id  + '/0.jpg" /></p><p>Date: ' + data[i].uploadDate + '</p>');
 
       $.each(data[i].songs, function (j, val) {
-        $('#output').append('<p>' + data[i].songs[j].artist + ' - <a href="' +data[i].videoLink + '?t=' + data[i].songs[j].timestamp + '">'  + data[i].songs[j].title + '</a></p>');
+        $('#output').append('<p>' + data[i].songs[j].artist + ' - <a href=https://youtu.be/' +data[i].id + '?t=' + data[i].songs[j].timestamp + '>'  + data[i].songs[j].title + '</a></p>');
       });
     });
   });
